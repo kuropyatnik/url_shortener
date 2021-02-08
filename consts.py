d625df4dfa3aad0c31df6b1c4ff9b64ea47e50a4
Danylo Kuropiatnyk <dkuropiatnyk@cloudlinux.com>
2021-02-08 22:15:48 +0200

    Adding env consts for DB

---
+++ consts.py
@@ -0,0 +1,8 @@
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+DB_HOST = os.getenv("DB_HOST")
+DB_USERNAME = os.getenv("DB_USERNAME")
+DB_PASSWD = os.getenv("DB_PASSWD")
+DB_NAME = os.getenv("DB_NAME")
--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from flask import Flask
+from consts import DB_HOST, DB_NAME
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    return f'Your db_host is {DB_HOST}, and db_name: {DB_NAME}'
 
 if __name__ == "__main__":
     app.run()
